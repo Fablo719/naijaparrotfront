e22b1efc50c922523720124f56649b38a72828b9
fablo <fabinuoluwadarasimi8@gmail.com>
2026-04-07 03:43:56 -0700

    correct alphabetical error

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,7 @@ import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./login.css";
+import "./Login.css";
 
 const Register = () => {
   const navigate = useNavigate();
