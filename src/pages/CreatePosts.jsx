d76feebecd321244699f842543339bb887148e0f
fablo <fabinuoluwadarasimi8@gmail.com>
2026-04-07 03:36:43 -0700

    fix errors

--- src/pages/CreatePosts.jsx
+++ src/pages/CreatePosts.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import './CreatePosts.css';
+import './Createposts.css';
 
 // 🔥 IMAGE COMPRESS FUNCTION
 const compressImage = (file) => {
